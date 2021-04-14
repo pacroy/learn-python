328a176c1ad1105d23bbd10e1d7e9e2280110cee
Par <pacroy@gmail.com>
2021-04-14 20:17:43 +0700

    Add range_example from lesson 7

---
+++ range_example.py
@@ -0,0 +1,7 @@
+print('My name is')
+for i in range(12, 16):
+    print('Jimmy Five Times ' + str(i))
+for i in range(0, 10, 2):
+    print('Jimmy Five Times ' + str(i))
+for i in range(10, 5, -1):
+    print('Jimmy Five Times ' + str(i))
