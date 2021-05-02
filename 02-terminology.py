442874f400bc1e319dbbc75a5c89961564a5336b
Par <pacroy@gmail.com>
2021-05-02 14:30:36 +0700

    Add 02-terminology

---
+++ 02-terminology.py
@@ -0,0 +1,22 @@
+# Expression 
+# = Value + Operator -> Will be evaluated down to a single value
+2 + 2
+(5-1)*((7+1)/(3-1))
+
+# Data types
+42              # Number
+3.14            # Float
+'Hello world!'  # String
+
+'Alice' + 'Bob'
+'Alice' * 3
+
+# Variables
+spam = 42   # Value assignment
+spam        # Variable is evaluated to its value
+spam = 'Hello'
+spam + ' world'
+
+# Statement
+# = aka. instruction or command -> Will NOT be evaluated to a single value and can have one or more expression
+spam = 2 + 2
