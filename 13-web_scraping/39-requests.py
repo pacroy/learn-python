27c6b86c00865fed8b9afd682b1d5c5238e9b04c
Par <pacroy@gmail.com>
2021-05-26 15:45:56 +0700

    Add video #39 requests download file

---
+++ 13-web_scraping/39-requests.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import requests
+
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+
+# raise_for_status() will raise an exception if there's any error
+res.raise_for_status()
+
+playFile = open('RomeoAndJuliet.txt', 'wb')
+for chunk in res.iter_content(100000):
+    playFile.write(chunk)
+
+playFile.close()
