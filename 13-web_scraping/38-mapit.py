0332e02f84cf3cef06ad8768aa61650709e4dd23
Par <pacroy@gmail.com>
2021-05-26 15:32:21 +0700

    Add video #38 webbrowser mapit

---
+++ 13-web_scraping/38-mapit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Open Google Map at given address
+# Usage: ./38-mapit.py <place to search>
+import webbrowser, sys, pyperclip
+
+if len(sys.argv) > 1:
+    address = ' '.join(sys.argv[1:])
+else:
+    address = pyperclip.paste()
+
+print('Address: ' + address)
+
+# webbrowser.open() opens web browser at specified URL
+webbrowser.open('https://www.google.co.th/maps/search/' + address)
