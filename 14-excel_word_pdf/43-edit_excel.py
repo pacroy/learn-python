f36e807a67c45f21a01bf20258a242e9974b0fb8
Par <pacroy@gmail.com>
2021-06-03 19:38:56 +0700

    Add video #43 edit excel file

---
+++ 14-excel_word_pdf/43-edit_excel.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# pip install openpyxl
+import openpyxl
+
+wb = openpyxl.Workbook()
+print(wb.sheetnames)
+sheet = wb["Sheet"]
+sheet["A1"].value = 42
+sheet["A2"].value = "Hello"
+
+sheet2 = wb.create_sheet("Sheet2")
+sheet.title = "Sheet1"
+print(wb.sheetnames)
+
+sheet3 = wb.create_sheet(index=0, title="New Sheet")
+print(wb.sheetnames)
+
+wb.save("test.xlsx")
