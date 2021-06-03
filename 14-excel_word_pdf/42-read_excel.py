718670256789e34ac09ca5623fc6854e6c71848a
Par <pacroy@gmail.com>
2021-06-03 19:24:31 +0700

    Add video #42 read Excel XLSX file

---
+++ 14-excel_word_pdf/42-read_excel.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# pip install openpyxl
+import openpyxl
+import os
+
+workbook = openpyxl.load_workbook("example.xlsx")
+print(f"workbook type={type(workbook)}")
+print(f"sheets={workbook.sheetnames}")
+
+
+sheet = workbook["Sheet1"]
+print(f"sheet Sheet1 type={type(sheet)}")
+
+cell = sheet["A1"]
+print(f"cell A1 type={type(cell.value)}")
+print(f"cell A1 value={str(cell.value)}")
+
+print(f"cell B1 value={sheet['B1'].value}")
+print(f"cell C1 value={sheet['C1'].value}")
+
+print(f"Cell row 1, columne 2 value={sheet.cell(row=1, column=2).value}")
+
+print("==============================")
+
+for i in range(1, 8):
+    print(i, sheet.cell(row=i, column=2).value)
+    
